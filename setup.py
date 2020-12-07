a108dd2144ca4dd5bec53646b11ea4103b1d0d6a
Kyro <50426223+kyro-dev@users.noreply.github.com>
2020-12-07 13:02:42 -0500

    packaged

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="quasar-firewall-kyro-dev",  # Replace with your own username
+    version="0.0.1",
+    author="Kyro Dev",
+    author_email="author@example.com",
+    description="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kyro-dev/Quasar-Project",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
